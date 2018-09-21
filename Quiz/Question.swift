2ebc89cbe5da083d078e4ea96bf5660735e5a8ca
RushianHaker <ivanov2001m352g@mail.ru>
2018-09-21 14:24:37 +0300

    created labels and buttons

---
+++ Quiz/Question.swift
@@ -0,0 +1,75 @@
+//
+//  Question.swift
+//  Quiz
+//
+//  Created by student on 21.09.2018.
+//  Copyright © 2018 Postnikov George. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    var text: String
+    var type: ResponceType
+    var answers: [Answer]
+}
+
+enum ResponceType {
+    case single, multiple, ranged
+}
+
+struct Answer {
+    var text: String
+    var type: AnimalType
+}
+
+enum AnimalType: Character {
+    case dog = "🐶", cat = "🐱", rabbit = "🐰", turtle = "🐢"
+    
+    var definition: String {
+        switch self {
+        case .dog:
+            return "Вам нравиться быть с друзьямы, вы общительны"
+        case .cat:
+            return "Вы себе на уме, любите быть в одиночестве"
+        case .rabbit:
+            return "Вы обожаете активный отдых и семью"
+        case .turtle:
+            return "Вы мудры и осторожны.  Медленный выигрывает."
+        }
+    }
+}
+
+var questions: [Question] = [
+    
+    Question(
+        text: "Какая пища вам нравится?",
+        type: .single,
+        answers: [
+            Answer(text: "Мясо", type: .dog),
+            Answer(text: "Рыба", type: .cat),
+            Answer(text: "Морковка", type: .rabbit),
+            Answer(text: "Кукуруза", type: .turtle)
+        ]),
+    
+    Question(
+        text: "Что вы любите делать?",
+        type: .multiple,
+        answers: [
+            Answer(text: "Есть", type: .dog),
+            Answer(text: "Спать", type: .cat),
+            Answer(text: "Обниматься", type: .rabbit),
+            Answer(text: "Плавать", type: .turtle)
+        ]),
+    
+    
+    Question(
+        text: "Насколько вы любите поездки?",
+        type: .ranged,
+        answers: [
+            Answer(text: "Ненавижу", type: .cat),
+            Answer(text: "Нервничаю", type: .rabbit),
+            Answer(text: "Не замечаю", type: .turtle),
+            Answer(text: "Обожаю", type: .dog)
+        ])
+]
--- Quiz/QuestionViewController.swift
+++ Quiz/QuestionViewController.swift
@@ -10,26 +10,51 @@ import UIKit
 
 class QuestionViewController: UIViewController {
 
+    var questionIndex = 0
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var singleStackView: UIStackView!
+    @IBOutlet weak var multipleStackView: UIStackView!
+    @IBOutlet weak var rangedStackView: UIStackView!
+    
+    @IBOutlet weak var singleButton1: UIButton!
+    @IBOutlet weak var singleButton2: UIButton!
+    @IBOutlet weak var singleButton3: UIButton!
+    @IBOutlet weak var singleButton4: UIButton!
+    
+    @IBOutlet weak var multiLabel1: UILabel!
+    @IBOutlet weak var multiLabel2: UILabel!
+    @IBOutlet weak var multiLabel3: UILabel!
+    @IBOutlet weak var multiLabel4: UILabel!
+    
+    @IBOutlet weak var rangedLabel1: UILabel!
+    @IBOutlet weak var rangedLabel2: UILabel!
+    
+    @IBOutlet weak var questionProgressView: UIProgressView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func updateUI() {
+        singleStackView.isHidden = true
+        multipleStackView.isHidden = true
+        rangedStackView.isHidden = true
+        
+        navigationItem.title = "Вопрос № \(questionIndex + 1)"
+        
+        let currentQuestion = questions[questionIndex]
+        switch currentQuestion.type {
+        case .single:
+            singleStackView.isHidden = false
+        case .multiple:
+            multipleStackView.isHidden = false
+        case .ranged:
+            rangedStackView.isHidden = false
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
